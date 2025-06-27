1dad0a65ae43b8cb8a11c87835f9f05f5bef9166
RobsonOliveiraSouza <robson.oliveira.souza21@gmail.com>
2025-06-27 09:12:42 -0300

    version 0.1

---
+++ src/main/java/com/bateai/bateai/BateaiApplication.java
@@ -0,0 +1,13 @@
+package com.bateai.bateai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BateaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BateaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bateai/bateai/BateaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bateai.bateai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BateaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
